5ecf263c91cb5c9777592b88599df0847141f964
Ge <georgiosamaras@hotmail.com>
2023-10-03 20:10:50 +0200

    post and profile view

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,22 +1,10 @@
 import { clerkClient } from "@clerk/nextjs";
-import type { User } from "@clerk/nextjs/dist/types/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
-
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-
-const filterUserForClient = (user: User) => {
-
-  return {
-      id: user.id,
-      username: user.username,
-      profilePictureUrl: user.imageUrl,
-    };
-};
-
-
 import { Ratelimit } from "@upstash/ratelimit"; 
 import { Redis } from "@upstash/redis"; 
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 // Create a new ratelimiter, that allows 10 requests per minute
 const ratelimit = new Ratelimit({
